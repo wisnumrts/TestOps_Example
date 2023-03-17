e87dcac39c5b5f97bfd0c725cfb65f612daf8dec
Syammary Wisnumurti <syammaryw@gmail.com>
2023-03-17 15:13:54 +0700

    add new test suites

---
+++ Test Suites/Products Feature.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Products Feature</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a290dffb-0c7f-4d81-b4e3-aff35ad68a48</testSuiteGuid>
+   <testCaseLink>
+      <guid>ca0b73a4-e20d-46b3-a435-5d5dc5455432</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Products Feature/PLI001 - User want to see products</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
