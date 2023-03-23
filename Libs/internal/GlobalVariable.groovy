78bf6e31772f1103bed12fed4dc89ff87af0d8f7
Syammary Wisnumurti <syammaryw@gmail.com>
2023-03-24 05:35:13 +0700

    add Take Screenshoot Test Case

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -14,7 +14,7 @@ public class GlobalVariable {
         try {
             def selectedVariables = TestCaseMain.getGlobalVariables("default")
 			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
-            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters())
+            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters(), selectedVariables)
     
             
         } catch (Exception e) {
--- Scripts/Take Screenshoot/Take a Screenshot as Checkpoint/Script1679610729954.groovy
+++ Scripts/Take Screenshoot/Take a Screenshot as Checkpoint/Script1679610729954.groovy
@@ -17,13 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('Step Definition/Login Feature/LGI005 - User want to login using correct credential'), [:], 
-    FailureHandling.STOP_ON_FAILURE)
-
-WebUI.callTestCase(findTestCase('Home Page/Click Product Image'), [:], FailureHandling.STOP_ON_FAILURE)
-
-WebUI.callTestCase(findTestCase('Product Detail/Verify Element'), [:], FailureHandling.STOP_ON_FAILURE)
-
-WebUI.callTestCase(findTestCase('Take Screenshoot/Take a Screenshot as Checkpoint'), [('imageName') : 'product detail'], 
-    FailureHandling.STOP_ON_FAILURE)
+WebUI.takeFullPageScreenshotAsCheckpoint(imageName)
 
